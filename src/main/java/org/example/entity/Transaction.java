df60d969c2e3d8282882a583976d7b41b3a2bc4b
P1cknikker <stefangeee@gmail.com>
2026-07-16 12:39:50 +0200

    Transaction changed

--- src/main/java/org/example/entity/Transaction.java
+++ src/main/java/org/example/entity/Transaction.java
@@ -1,4 +1,17 @@
 package org.example.entity;
 
 public class Transaction {
+    int id;
+    int accountId;
+    String type;
+    int amount;
+    int timestamp;
+
+    public Transaction(int id, int accountId, String type, int amount, int timestamp){
+        this.id = id;
+        this.accountId = accountId;
+        this.type = type;
+        this.amount = amount;
+        this.timestamp =timestamp;
+    }
 }
