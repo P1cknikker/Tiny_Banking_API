6dd089514f0c537ea68a5caa8380f765f02854b6
P1cknikker <stefangeee@gmail.com>
2026-07-16 12:30:27 +0200

    Accounts changed

--- src/main/java/org/example/entity/Account.java
+++ src/main/java/org/example/entity/Account.java
@@ -1,4 +1,15 @@
 package org.example.entity;
 
 public class Account {
+    int id;
+    char iban;
+    int balance;
+    int customerId;
+
+    public Account(int id, char iban, int balance, int customerId ){
+        this.id = id;
+        this.iban = iban;
+        this.balance = balance;
+        this.customerId = customerId;
+    }
 }
